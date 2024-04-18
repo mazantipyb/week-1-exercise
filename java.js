e65d1c19c5e7f4a8f413003b2cd612db64f6bf37
mazantipyb <165015267+mazantipyb@users.noreply.github.com>
2024-04-17 23:31:52 -0700

    Calculator

---
+++ java.js
@@ -0,0 +1,49 @@
+function addnumbers (){
+    let firstNumber = parseFloat(document.getElementById("first").value);
+    let secondNumber = parseFloat(document.getElementById("second").value);
+    let total = firstNumber + secondNumber;
+    alert(total);
+}
+function substractnumbers (){
+    let firstNumber = parseFloat(document.getElementById("first").value);
+    let secondNumber = parseFloat(document.getElementById("second").value);
+    let total = firstNumber - secondNumber;
+    alert(total);
+}
+
+function dividenumbers (){
+    let firstNumber = parseFloat(document.getElementById("first").value);
+    let secondNumber = parseFloat(document.getElementById("second").value);
+    let total = firstNumber / secondNumber;
+    alert(total);
+}
+
+function multiplynumbers (){
+    let firstNumber = parseFloat(document.getElementById("first").value);
+    let secondNumber = parseFloat(document.getElementById("second").value);
+    let total = firstNumber * secondNumber;
+    alert(total("the remainder when {firstNumber}is diveded by {secondNumber}is :{remainder}"));
+}
+
+/*function dividenumbers(){
+    let firstNumber = parseFloat(document.getElementById("first").value);
+    let secondNumber = parseFloat(document.getElementById("second").value);
+    let remainder = firstNumber % secondNumber;
+    alert("the remainder when {firstNumber}is diveded by {secondNumber}is :{remainder}");
+}*/
+    
+    
+
+
+
+
+ /*let firstNumber = parseFloat(prompt('enter your firstNumber'));
+ let secondNumber = parseFloat(prompt('enter your secondNumber'));
+ let total = firstNumber + secondNumber;
+ console.log(total);*/
+
+
+
+
+
+
